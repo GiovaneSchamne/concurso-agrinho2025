827a90c4174fd13eb7658e5d02e5aee1182d5458
GiovaneSchamne <142627791+GiovaneSchamne@users.noreply.github.com>
2025-06-27 21:19:01 -0300

    Update scrypt.js

--- scrypt.js
+++ scrypt.js
@@ -1,13 +1,13 @@
 <script>
-  function verificarRespostas() {
+  function verificarRespostas() 
     const respostas = document.querySelectorAll('input[type="radio"]:checked');
     let acertos = 0;
 
-    respostas.forEach(resposta = {
+    respostas.forEach()resposta = 
       if (resposta.value === "certo") {
         acertos++
       }
-    {});
+    {};
 
     document.getElementById("resultado").{textContent =
       `Você acertou ${acertos} de 3 perguntas! 👏`
